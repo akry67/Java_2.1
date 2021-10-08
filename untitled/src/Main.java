9de8d3ad27b95656e3db92f70ca23422f2998066
Anna Kuznetsova <akry67@gmail.com>
2021-10-08 18:12:16 +0400

    bonusMiles

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int rubleMile = 20;
+        long ticketsPrice = 2000;
+        long bonusMiles = ticketsPrice/ rubleMile;
+
+        System.out.println(bonusMiles);
+    }
+
 }
